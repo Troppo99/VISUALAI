e8a99b2c5c55fa9b1ec636617e14b34590344d26
Troppo99 <troppolungo99@gmail.com>
2024-12-13 16:24:17 +0700

    -

--- src/.run.py
+++ src/.run.py
@@ -1,19 +1,19 @@
 import time
 from Scheduling import Scheduling
 
+if __name__ == "__main__":
+    detector_args = {
+        "confidence_threshold": 0.5,
+        "camera_name": "OFFICE3",
+        # "video_source": "static/videos/bd_test3.mp4",
+        "window_size": (320, 240),
+    }
 
-detector_args = {
-    "confidence_threshold": 0.5,
-    "camera_name": "OFFICE3",
-    # "video_source": "static/videos/bd_test3.mp4",
-    "window_size": (320, 240),
-}
+    scheduler = Scheduling(detector_args)
 
-scheduler = Scheduling(detector_args)
-
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Program terminated by user.")
-    scheduler.shutdown()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
+        scheduler.shutdown()
