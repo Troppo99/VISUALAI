45cd9867df6b3433507cb3aed4c5e5443f3f701f
Troppo99 <troppolungo99@gmail.com>
2024-12-23 16:04:29 +0700

    hapus django project

--- website/stream/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    context = {
-        "title": "VisualAI",
-        "heading": "This is AI Streaming",
-    }
-    return render(request, "stream/index.html", context)
