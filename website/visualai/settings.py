55d2c06e184f2099163a3ed498a0a86957c016c6
Troppo99 <troppolungo99@gmail.com>
2024-12-26 11:27:54 +0700

    #dj setting model engine

--- website/visualai/settings.py
+++ website/visualai/settings.py
@@ -68,8 +68,15 @@ WSGI_APPLICATION = "visualai.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "visualai_db",
+        "USER": "root",
+        "PASSWORD": "robot123",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
