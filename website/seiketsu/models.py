6eb8471bf06969610dbc7876fe986265e07044fd
Troppo99 <troppolungo99@gmail.com>
2024-12-26 11:55:07 +0700

    #dj coba model dan admin

--- website/seiketsu/models.py
+++ website/seiketsu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class check(models.Model):
+  id = models.AutoField(primary_key=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  message = models.CharField(max_length=100)
+
+  def __str__(self):
+    return "{}".format(self.message)
