bf21196f9df05521de3cb24e8a8657f27fb60529
Troppo99 <troppolungo99@gmail.com>
2024-12-27 10:23:05 +0700

    -

--- website/seiketsu/views.py
+++ website/seiketsu/views.py
@@ -3,7 +3,15 @@ from .src.contopdetector import ContopDetector
 from django.http import StreamingHttpResponse, Http404
 from django.contrib.staticfiles import finders
 
-CAMERA_NAMES = ["FREEMETAL1", "FREEMETAL2", "METALDET1", "FOLDING1", "FOLDING2", "FOLDING3", "SEWINGBACK2"]
+CAMERA_NAMES = [
+    "FREEMETAL1",
+    "FREEMETAL2",
+    "METALDET1",
+    "FOLDING1",
+    "FOLDING2",
+    "FOLDING3",
+    "SEWINGBACK2",
+]
 
 
 def index(request):
