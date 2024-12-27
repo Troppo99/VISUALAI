86c4407a247f9cb83955e7fcda731bf6d75db613
Troppo99 <troppolungo99@gmail.com>
2024-12-27 07:39:12 +0700

    #dj adjust display

--- website/seiketsu/views.py
+++ website/seiketsu/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from .src.contopdetector import ContopDetector
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, Http404
 from django.contrib.staticfiles import finders
+
+CAMERA_NAMES = ["FREEMETAL1", "FREEMETAL2", "METALDET1", "FOLDING1", "FOLDING2", "FOLDING3", "SEWINGBACK2"]
 
 
 def index(request):
-    video_count = 11
-    videos = range(1, video_count + 1)
-    return render(request, "seiketsu/index.html", {"videos": videos})
+    cameras = CAMERA_NAMES
+    return render(request, "seiketsu/index.html", {"cameras": cameras})
 
 
-def seiketsu_feed(request):
+def seiketsu_feed(request, camera_name):
+    if camera_name not in CAMERA_NAMES:
+        raise Http404("Camera not found")
+
     detector = ContopDetector(
-        camera_name="FREEMETAL1",
+        camera_name=camera_name,
         # video_source=finders.find("videos/contop testing.mp4"),
     )
     return StreamingHttpResponse(detector.stream_frames(), content_type="multipart/x-mixed-replace; boundary=frame")
