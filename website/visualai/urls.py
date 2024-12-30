846cf8cb79a8290f78708544d173ab20d8dff66c
Troppo99 <troppolungo99@gmail.com>
2024-12-30 11:10:48 +0700

    #dj hapus core

--- website/visualai/urls.py
+++ website/visualai/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path("", views.index, name="index"),
+    path("seiri_page/", include("seiri.urls")),
+    path("seiton_page/", include("seiton.urls")),
+    path("seiso_page/", include("seiso.urls")),
+    path("seiketsu_page/", include("seiketsu.urls")),
+    # path("shitsuke_page/", include("shitsuke.urls")),
 ]
 
 
