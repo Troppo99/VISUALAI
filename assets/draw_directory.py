aadd48200f643f0751f88ad3ef6d19e2f557909f
Troppo99 <troppolungo99@gmail.com>
2024-12-30 13:24:48 +0700

    masih ada kejanggalan

--- assets/draw_directory.py
+++ assets/draw_directory.py
@@ -15,7 +15,7 @@ def print_directory_tree(root_dir, indent=""):
 
 
 def main():
-    root_folder = r"D:\NWR\datasets\contop_united"
+    root_folder = r"C:\xampp\htdocs\VISUALAI\website\templates"
     if not os.path.exists(root_folder):
         print(f"Folder sumber '{root_folder}' tidak ditemukan.")
         return
--- website/visualai/settings.py
+++ website/visualai/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "five_s",
+    "fives",
     "seiketsu",
 ]
 
