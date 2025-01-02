abec93f42848a666b2287b1ae60f55b9455b1ce2
Troppo99 <troppolungo99@gmail.com>
2025-01-02 13:05:56 +0700

    direktori

--- website/visualai/views.py
+++ website/visualai/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import StreamingHttpResponse, Http404
-from five_s.src.contopdetector import ContopDetector
+from five_s.src.dist.contopdetector import ContopDetector
 
 CAMERA_NAMES = [
     "FREEMETAL1",
