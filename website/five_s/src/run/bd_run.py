70ccca3472c618a5c343beb896e401fbe8e3641b
Troppo99 <troppolungo99@gmail.com>
2025-01-06 12:26:31 +0700

    #src bd run pindah folder

---
+++ website/five_s/src/run/bd_run.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.join(current_dir, "..")
+    sys.path.append(parent_dir)
+    from tests.bd_test import BroomDetector
+
+    detector_args = {
+        "confidence_threshold": 0,
+        "camera_name": "SEWING1",
+        # "video_source": r"C:\xampp\htdocs\VISUALAI\archives\static\videos\bd_test.mp4",
+        "window_size": (320, 240),
+    }
+
+    detector = BroomDetector(**detector_args)
+    detector.main()
