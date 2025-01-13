a2cc08d59341aa15220a572f1a747ac78b089641
Troppo99 <troppolungo99@gmail.com>
2025-01-13 07:47:43 +0700

    -

--- assets/train.py
+++ assets/train.py
@@ -3,11 +3,11 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     model = YOLO(r"C:\xampp\htdocs\VISUALAI\website\static\resources\models\yolo11l.pt")
     model.train(
-        data=r"C:\xampp\htdocs\VISUALAI\website\static\resources\datasets\blazing\data.yaml",
+        data=r"C:\xampp\htdocs\VISUALAI\website\static\resources\datasets\spreading\data.yaml",
         epochs=100,
         imgsz=640,
         project=r"C:\xampp\htdocs\VISUALAI\website\static\resources\models",
-        name="blazing",
+        name="spreading",
         device="cuda",
         batch=16,
         resume=False,
