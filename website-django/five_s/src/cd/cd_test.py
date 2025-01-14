cf7f37bd251d15ed01d92c6b51c26ba84414506b
Troppo99 <troppolungo99@gmail.com>
2025-01-14 14:39:01 +0700

    -

--- website-django/five_s/src/cd/cd_test.py
+++ website-django/five_s/src/cd/cd_test.py
@@ -10,13 +10,21 @@ from libs.DataHandler import DataHandler
 
 
 class CarpalDetector:
-    def __init__(self, confidence_threshold=0.5, video_source=None, camera_name=None, window_size=(540, 360)):
+    def __init__(self, confidence_threshold=0.5, video_source=None, camera_name=None, window_size=(540, 360), stop_event=None):
+        self.stop_event = stop_event
+        if self.stop_event is None:
+            self.stop_event = threading.Event()
         self.confidence_threshold = confidence_threshold
         self.video_source = video_source
         self.camera_name = camera_name
         self.window_size = window_size
         self.process_size = (640, 640)
         self.rois, self.ip_camera = self.camera_config()
+        self.choose_video_source()
+        self.prev_frame_time = 0
+        self.model = YOLO(r"C:\xampp\htdocs\VISUALAI\website-django\static\resources\models\yolo11l-pose.pt").to("cuda")
+        self.model.overrides["verbose"] = False
+
         if len(self.rois) > 1:
             self.union_roi = unary_union(self.rois)
         elif len(self.rois) == 1:
@@ -26,18 +34,13 @@ class CarpalDetector:
 
         self.trail_map_polygon = Polygon()
         self.trail_map_mask = np.zeros((self.process_size[1], self.process_size[0], 3), dtype=np.uint8)
-        self.start_run_time = time.time()
+        
         self.last_detection_time = None
         self.trail_map_start_time = None
+        self.start_run_time = time.time()
         self.capture_done = False
-        self.frame_queue = None
-        self.choose_video_source()
-        self.prev_frame_time = 0
-        self.model = YOLO(r"C:\xampp\htdocs\VISUALAI\website-django\static\resources\models\yolo11l-pose.pt").to("cuda")
-        self.model.overrides["verbose"] = False
-        self.stop_event = threading.Event()
+
         self.pairs_human = [(0, 1), (0, 2), (1, 2), (2, 4), (1, 3), (4, 6), (3, 5), (5, 6), (6, 8), (8, 10), (5, 7), (7, 9), (6, 12), (12, 11), (11, 5), (12, 14), (14, 16), (11, 13), (13, 15)]
-        self.final_overlap = 0
 
     def camera_config(self):
         with open(r"C:\xampp\htdocs\VISUALAI\website-django\static\resources\conf\camera_config.json", "r") as f:
