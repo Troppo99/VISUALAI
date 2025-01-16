005100d5327414368ad8252d68bd05eaff6a1054
Troppo99 <troppolungo99@gmail.com>
2025-01-16 14:06:02 +0700

    dist

--- dist/tests/bd_play-itachi.py
+++ dist/tests/bd_play-itachi.py
@@ -3,28 +3,28 @@ import subprocess
 camera_list = [
     # "BUFFER1",
     # "CUTTING3",
-    # "EKSPEDISI1",
+    # "EKSPEDISI2",
     # "FOLDING2",
     # "FOLDING3",
     # "FREEMETAL1",
-    "FREEMETAL2",
-    "GUDANGACC1",
-    "GUDANGACC2",
-    "GUDANGACC3",
-    "GUDANGACC4",
-    "INNERBOX1",
-    "KANTIN1",
+    # "FREEMETAL2",
+    # "GUDANGACC1",
+    # "GUDANGACC2",
+    # "GUDANGACC3",
+    # "GUDANGACC4",
+    # "INNERBOX1",
+    # "KANTIN1",
     # "LINEMANUAL10",
     # "LINEMANUAL14",
     # "LINEMANUAL15",
     # "METALDET1",
-    # "SEWING1",
-    # "SEWING2",
-    # "SEWING3",
-    # "SEWING4",
-    # "SEWING5",
-    # "SEWING6",
-    # "SEWING7",
+    "SEWING1",
+    "SEWING2",
+    "SEWING3",
+    "SEWING4",
+    "SEWING5",
+    "SEWING6",
+    "SEWING7",
     # "SEWINGBACK1",
     # "SEWINGBACK2",
     # "SEWINGOFFICE",
@@ -32,7 +32,7 @@ camera_list = [
 
 processes = []
 for cam in camera_list:
-    p = subprocess.Popen([".venv\\Scripts\\python.exe", f"tests\\bd_run-{cam}.py"])
+    p = subprocess.Popen([".venv\\Scripts\\python.exe", f"bd\\run-{cam}.py"])
     processes.append(p)
 
 for p in processes:
