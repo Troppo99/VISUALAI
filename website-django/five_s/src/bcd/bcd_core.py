a3b044b6c14c115058c87572dd230122b27330b2
Troppo99 <troppolungo99@gmail.com>
2025-01-17 09:26:32 +0700

    capture frame

--- website-django/five_s/src/bcd/bcd_core.py
+++ website-django/five_s/src/bcd/bcd_core.py
@@ -44,13 +44,62 @@ class BroCarpDetector:
         self.pairs_human = [(0, 1), (0, 2), (1, 2), (2, 4), (1, 3), (4, 6), (3, 5), (5, 6), (6, 8), (8, 10), (5, 7), (7, 9), (6, 12), (12, 11), (11, 5), (12, 14), (14, 16), (11, 13), (13, 15)]
 
     def camera_config(self):
-        pass
+        with open(r"\\10.5.0.3\VISUALAI\website-django\static\resources\conf\camera_config.json", "r") as f:
+            config = json.load(f)
+        ip = config[self.camera_name]["ip"]
+        scaled_rois = []
+        rois_path = config[self.camera_name]["bcd_rois"]
+        with open(rois_path, "r") as rois_file:
+            original_rois = json.load(rois_file)
+        for roi_group in original_rois:
+            scaled_group = []
+            for x, y in roi_group:
+                scaled_x = int(x * (960 / 1280))
+                scaled_y = int(y * (540 / 720))
+                scaled_group.append((scaled_x, scaled_y))
+            if len(scaled_group) >= 3:
+                polygon = Polygon(scaled_group)
+                if polygon.is_valid:
+                    scaled_rois.append(polygon)
+        return scaled_rois, ip
 
     def choose_video_source(self):
-        pass
+        if self.video_source is None:
+            self.frame_queue = queue.Queue(maxsize=10)
+            self.frame_thread = None
+            self.video_fps = None
+            self.is_local_video = False
+            self.video_source = f"rtsp://admin:oracle2015@{self.ip_camera}:554/Streaming/Channels/1"
+        else:
+            if os.path.isfile(self.video_source):
+                self.is_local_video = True
+                cap = cv2.VideoCapture(self.video_source)
+                self.video_fps = cap.get(cv2.CAP_PROP_FPS)
+                if not self.video_fps or math.isnan(self.video_fps):
+                    self.video_fps = 25
+                cap.release()
+            else:
+                self.is_local_video = False
+                self.video_fps = None
 
-    def camera_capture(self):
-        pass
+    def capture_frame(self):
+        while not self.stop_event.is_set():
+            cap = cv2.VideoCapture(self.video_source)
+            if not cap.isOpened():
+                cap.release()
+                time.sleep(5)
+                continue
+            while not self.stop_event.is_set():
+                ret, frame = cap.read()
+                if not ret:
+                    cap.release()
+                    time.sleep(5)
+                    break
+                try:
+                    self.frame_queue.put(frame, timeout=1)
+                except queue.Full:
+                    pass
+            cap.release()
 
     def export_frame(self):
         pass
