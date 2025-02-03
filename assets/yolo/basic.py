52fae8aacc615f26c1c15172e0e0dedc535cbb8c
Troppo99 <troppolungo99@gmail.com>
2025-02-03 14:15:30 +0700

    menambahkan segment pada basic

--- assets/yolo/basic.py
+++ assets/yolo/basic.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 
 class YoloInference:
-    def __init__(self, confidence_threshold=0, video_source=None, camera_name=None, stop_event=None):
+    def __init__(self, confidence_threshold=0, video_source=None, camera_name=None, stop_event=None, where_model=None, where_task="detect"):
         self.stop_event = stop_event if stop_event else threading.Event()
         self.confidence_threshold = confidence_threshold
         self.video_source = video_source
@@ -14,10 +14,12 @@ class YoloInference:
             self.video = self.camera_config()
         else:
             self.video = camera_name
+        self.where_task = where_task
+        self.where_model = where_model
         self.choose_video_source()
         self.prev_frame_time = 0
-        self.model_detect = YOLO(r"C:\xampp\htdocs\VISUALAI\website-django\inspection\static\resources\models\defect1-seg-960\weights\best.pt")
-        # self.model_detect.overrides["verbose"] = False
+        model_path = where_model if where_model else r"C:\xampp\htdocs\VISUALAI\website-django\inspection\static\resources\models\defect1-seg-960\weights\best.pt"
+        self.model_detect = YOLO(model_path)
         self.yolo_task = YoloTask(self.model_detect, self.confidence_threshold, self.process_size)
 
     def camera_config(self):
@@ -66,17 +68,22 @@ class YoloInference:
 
     def process_frame(self, frame):
         frame_resized = cv2.resize(frame, self.process_size)
-        boxes = self.yolo_task.export_frame_detect(frame_resized)
-        output_frame = frame_resized.copy()
-        overlay = output_frame.copy()
-        for x1, y1, x2, y2, class_id in boxes:
-            y1 -= 30
-            y2 += 30
-            x1 -= 10
-            x2 += 10
-            cvzone.cornerRect(overlay, (x1, y1, x2 - x1, y2 - y1), l=40, rt=0, t=5, colorC=(0, 0, 255))
-            cvzone.putTextRect(overlay, f"{class_id}", (x1, y1), scale=3, thickness=3, offset=5)
-        return cv2.addWeighted(overlay, 0.5, output_frame, 0.5, 0)
+        if self.where_task == "detect":
+            boxes = self.yolo_task.export_frame_detect(frame_resized)
+            output_frame = frame_resized.copy()
+            overlay = output_frame.copy()
+            for x1, y1, x2, y2, class_id in boxes:
+                y1 -= 30
+                y2 += 30
+                x1 -= 10
+                x2 += 10
+                cvzone.cornerRect(overlay, (x1, y1, x2 - x1, y2 - y1), l=40, rt=0, t=5, colorC=(0, 0, 255))
+                # cvzone.putTextRect(overlay, f"{class_id}", (x1, y1), scale=3, thickness=3, offset=5)
+            return cv2.addWeighted(overlay, 0.5, output_frame, 0.5, 0)
+        elif self.where_task == "segment":
+            return self.yolo_task.export_frame_segment(frame_resized)
+        else:
+            return frame_resized
 
     def main(self):
         skip_frames = 2
@@ -84,14 +91,12 @@ class YoloInference:
         window_name = f"DFD:{self.camera_name}"
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(window_name, self.window_size)
-
         try:
             if self.video_fps is None:
                 self.frame_queue = queue.Queue(maxsize=10)
                 self.frame_thread = threading.Thread(target=self.capture_frame)
                 self.frame_thread.daemon = True
                 self.frame_thread.start()
-
                 while not self.stop_event.is_set():
                     try:
                         frame = self.frame_queue.get(timeout=1)
@@ -104,9 +109,9 @@ class YoloInference:
                     time_diff = current_time - self.prev_frame_time
                     self.fps = 1 / time_diff if time_diff > 0 else 0
                     self.prev_frame_time = current_time
-                    frame_resized = self.process_frame(frame)
-                    cvzone.putTextRect(frame_resized, f"FPS: {int(self.fps)}", (10, 90), scale=3, thickness=3, offset=5)
-                    frame_show = cv2.resize(frame_resized, self.window_size)
+                    frame_processed = self.process_frame(frame)
+                    cvzone.putTextRect(frame_processed, f"FPS: {int(self.fps)}", (10, 90), scale=3, thickness=3, offset=5)
+                    frame_show = cv2.resize(frame_processed, self.window_size)
                     cv2.imshow(window_name, frame_show)
                     if cv2.waitKey(1) & 0xFF in [ord("n"), ord("N")]:
                         self.stop_event.set()
@@ -130,10 +135,9 @@ class YoloInference:
                     time_diff = current_time - self.prev_frame_time
                     self.fps = 1 / time_diff if time_diff > 0 else 0
                     self.prev_frame_time = current_time
-                    frame_resized = self.process_frame(frame)
-                    cvzone.putTextRect(frame_resized, f"FPS: {int(self.fps)}", (10, 90), scale=3, thickness=3, offset=5)
-
-                    frame_show = cv2.resize(frame_resized, self.window_size)
+                    frame_processed = self.process_frame(frame)
+                    cvzone.putTextRect(frame_processed, f"FPS: {int(self.fps)}", (10, 90), scale=3, thickness=3, offset=5)
+                    frame_show = cv2.resize(frame_processed, self.window_size)
                     cv2.imshow(window_name, frame_show)
                     processing_time = (time.time() - start_time) * 1000
                     adjusted_delay = max(int(frame_delay - processing_time), 1)
@@ -166,7 +170,28 @@ class YoloTask:
         return boxes
 
     def export_frame_segment(self, frame):
-        pass
+        with torch.no_grad():
+            results = self.model(frame, stream=True, imgsz=self.process_size[0])
+        overlay = frame.copy()
+        for result in results:
+            if hasattr(result, "masks") and result.masks is not None:
+                masks = result.masks.data.cpu().numpy()
+                cls_list = []
+                if result.boxes is not None:
+                    for cls in result.boxes.cls:
+                        cls_list.append(self.model.names[int(cls)])
+                else:
+                    cls_list = ["unknown"] * masks.shape[0]
+                for i, mask in enumerate(masks):
+                    mask = (mask > 0.5).astype("uint8") * 255
+                    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    color = (0, 255, 0)
+                    cv2.drawContours(overlay, contours, -1, color, thickness=cv2.FILLED)
+                    if contours:
+                        cnt = max(contours, key=cv2.contourArea)
+                        x, y, w, h = cv2.boundingRect(cnt)
+                        # cvzone.putTextRect(overlay, f"{cls_list[i]}", (x, y), scale=3, thickness=3, offset=5)
+        return cv2.addWeighted(overlay, 0.5, frame, 0.5, 0)
 
     def export_frame_classify(self, frame):
         pass
@@ -180,8 +205,10 @@ class YoloTask:
 
 if __name__ == "__main__":
     yi = YoloInference(
+        confidence_threshold=0.75,
         camera_name="ROBOTICS",
-        # video_source=r"C:\xampp\htdocs\VISUALAI\website-django\inspection\static\videos\test3.mp4",
         video_source=1,
+        where_model=r"C:\xampp\htdocs\VISUALAI\website-django\inspection\static\resources\models\defect-strip\weights\best.pt",
+        where_task="segment",
     )
     yi.main()
